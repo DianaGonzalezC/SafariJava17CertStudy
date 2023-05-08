37ca046e7013560ba5b9daa21eb4ccad0e376709
simon <simon@dancingcloudservices.com>
2023-05-08 09:23:29 -0600

    Day 1 break 1

---
+++ src/main/java/usingvar/Ex1.java
@@ -0,0 +1,48 @@
+package usingvar;
+
+//class X {
+//  var x = 99;
+//}
+
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class Ex1 {
+//  void doStuff(var x) { }
+//  void doStuff() {
+//    var x;
+//    x = 100;
+//  }
+//  void doStuff() {
+//    var x = 100;
+//  }
+//  void doStuff() {
+//    var x = null;
+//  }
+
+  public static void main(String[] args) throws Throwable {
+//    for (var x = 0; x < 3; x++)
+//      System.out.println(x);
+
+//    try (var in = new FileReader("");
+//         var out = new FileWriter("")) {
+//    }
+
+//    try (FileReader in = new FileReader("")) {
+//    } catch (var ex) { }
+
+  }
+
+//  void doStuff() {
+//    var x = new int[]{ 1, 2, 3 };
+//  }
+
+//  void doStuff() {
+//    var x = { 1, 2, 3 };
+//  }
+
+//  void showStuff(CharSequence [] csa) {}
+//  void useIt() {
+//    showStuff(new String[]{"a", "b"});
+//  }
+}
