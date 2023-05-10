995e08baf4c39b3761b652625326848555e4e4b1
simon <simon@dancingcloudservices.com>
2023-05-10 08:56:09 -0600

    Day 3 break 1

--- src/main/java/init2/Q.java
+++ src/main/java/init2/Q.java
@@ -1,4 +1,4 @@
-package initialization;
+package init2;
 
 import static java.lang.System.out;
 
@@ -12,8 +12,7 @@ class Parent {
 class Child extends Parent {
   int x = 200;
   static int y = 300;
-//  Child() { super(x); }
-  Child() { super(100); } // avoid the compilation failure!
+  Child() { super(y); }
   Child(int x) { this(); }
   { out.print(", C-i x: is " + x); }
   static { out.print(", C-si: y is " + y); }
@@ -25,12 +24,5 @@ class InitOrder3 {
     out.println(" Goodbye!");
   }
 }
-//What is the result?
-//A) Compilation fails
-//B) Exception at runtime
-//C) Hello!, P-si: x is 0, C-si: y is 300, P-i: y is 101, P-c: y is 401, C-i x: is 200 Goodbye!
-//D) , P-si: x is 0, C-si: y is 300Hello!, P-i: y is 101, P-c: y is 401, C-i x: is 200 Goodbye!
-//E) Hello!, P-si: x is 99, C-si: y is 300, P-i: y is 101, P-c: y is 100Goodbye!
-
-public class Example {
+public class Q {
 }
